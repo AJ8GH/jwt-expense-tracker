5d9dac82df00babc4c7854e6d1cf6a6d5ca56f0a
Adam Jonas <adamjonas25@gmail.com>
2025-06-17 08:11:15 +0100

    Set up postgres

--- app/src/test/kotlin/io/github/aj8gh/expenses/ComponentSpec.kt
+++ app/src/test/kotlin/io/github/aj8gh/expenses/ComponentSpec.kt
@@ -1,22 +1,27 @@
 package io.github.aj8gh.expenses
 
 import io.github.aj8gh.expenses.config.TestClientConfig
+import io.github.aj8gh.expenses.config.TestDatabaseConfig
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Import(TestClientConfig::class)
+@AutoConfigureEmbeddedDatabase(type = POSTGRES, provider = ZONKY)
+@Import(value = [TestClientConfig::class, TestDatabaseConfig::class])
 class ComponentSpec(
   private val client: OkHttpClient,
-  @Value("\${local.server.port}") private val port: Int,
+  @LocalServerPort private val port: Int,
 ) : FunSpec({
 
   test("should load context") {
